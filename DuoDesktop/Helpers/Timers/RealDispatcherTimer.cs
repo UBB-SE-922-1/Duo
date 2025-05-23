251aa60539b7438818412368e7da54786d69a806
dosqas <137123562+dosqas@users.noreply.github.com>
2025-05-23 13:28:37 +0300

    Add RealDispatcherTimer class for UWP timer functionality
    Introduces the `RealDispatcherTimer` class in the `DuoDesktop.Helpers.Timers` namespace. This class wraps the UWP `DispatcherTimer` and implements the `IDispatcherTimer` interface, providing a constructor for optional testing. It includes an event `Tick`, properties for `Interval`, and methods to `Start` and `Stop` the timer. The class is marked with `[ExcludeFromCodeCoverage]` due to its reliance on the Windows Runtime, which complicates testing.

---
+++ DuoDesktop/Helpers/Timers/RealDispatcherTimer.cs
@@ -0,0 +1,73 @@
+﻿namespace DuoDesktop.Helpers.Timers
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using DuoDesktop.Helpers.Interfaces;
+    using Microsoft.UI.Xaml;
+
+    /// <summary>
+    /// Wrapper class for the UWP DispatcherTimer that implements our custom IDispatcherTimer interface.
+    /// This class is excluded from code coverage since it cannot be properly tested/mocked due to its
+    /// tight coupling with the Windows Runtime (WinRT) in UWP, which causes COM exceptions during testing.
+    /// </summary>
+    /// <remarks>
+    /// Initializes a new instance of the RealDispatcherTimer class
+    /// </remarks>
+    /// <param name="dispatcherTimer">
+    /// Optional DispatcherTimer instance to wrap. If null, creates a new instance.
+    /// This parameter exists primarily to support testing scenarios.
+    /// </param>
+    [ExcludeFromCodeCoverage]
+    public class RealDispatcherTimer(IDispatcherTimer? dispatcherTimer = null) : IDispatcherTimer
+    {
+
+        #region Constructor
+
+        #endregion
+
+        #region Fields
+        private readonly DispatcherTimer dispatcherTimer = dispatcherTimer as DispatcherTimer ?? new DispatcherTimer();
+        #endregion
+
+        #region Events
+
+        /// <summary>
+        /// Event that occurs when the timer interval has elapsed
+        /// </summary>
+        /// <remarks>
+        /// The event signature is adapted to match EventHandler&lt;object&gt; to maintain
+        /// compatibility with both the UWP DispatcherTimer and our mockable interface
+        /// </remarks>
+        public event EventHandler<object> Tick
+        {
+            add => dispatcherTimer.Tick += (sender, e) => value(sender, e);
+            remove => dispatcherTimer.Tick -= (sender, e) => value(sender, e);
+        }
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the amount of time between timer ticks
+        /// </summary>
+        public TimeSpan Interval
+        {
+            get => dispatcherTimer.Interval;
+            set => dispatcherTimer.Interval = value;
+        }
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Starts the timer
+        /// </summary>
+        public void Start() => dispatcherTimer.Start();
+
+        /// <summary>
+        /// Stops the timer
+        /// </summary>
+        public void Stop() => dispatcherTimer.Stop();
+        #endregion
+    }
+}
