b6d2ed7c160f59a7dd9fc72b0f22247c2a478304
tiutcristian <107301343+tiutcristian@users.noreply.github.com>
2025-05-25 22:12:19 +0300

    Implement Button handler

--- Duo/Views/Pages/MainPage.xaml.cs
+++ Duo/Views/Pages/MainPage.xaml.cs
@@ -1,4 +1,7 @@
+using Duo.ViewModels;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using System.Diagnostics;
 
 namespace Duo.Views.Pages
 {
@@ -7,6 +10,25 @@ namespace Duo.Views.Pages
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Logout button handler
+        /// </summary>
+        public void LogoutButton_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+        {
+            ((MainPageViewModel)App.ServiceProvider.GetService(typeof(MainPageViewModel))).HandleLogoutClick();
+            // get parent frame
+            Frame parentFrame = this.Frame;
+            if (parentFrame != null)
+            {
+                // Navigate to LoginPage
+                parentFrame.Navigate(typeof(LoginPage));
+            }
+            else
+            {
+                Debug.WriteLine("Parent frame is null, cannot navigate to LoginPage.");
+            }
         }
     }
 }
