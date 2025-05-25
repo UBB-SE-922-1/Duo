b3a032342f80ebca2b26df7368f254811e4ae21b
Vlad <vlad.cenuse@gmail.com>
2025-05-25 16:55:32 +0300

    Kinda stricat

--- WebServerTest/Program.cs
+++ WebServerTest/Program.cs
@@ -23,21 +23,7 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-// Add HttpClient configurations
-builder.Services.AddHttpClient<IQuizServiceProxy, QuizServiceProxy>(client =>
-{
-    client.BaseAddress = new Uri(apiBase);
-});
-
-builder.Services.AddHttpClient<IExerciseServiceProxy, ExerciseServiceProxy>(client =>
-{
-    client.BaseAddress = new Uri(apiBase);
-});
-
-builder.Services.AddHttpClient<ISectionServiceProxy, SectionServiceProxy>(client =>
-{
-    client.BaseAddress = new Uri(apiBase);
-});
+builder.Services.AddHttpClient();
 
 // Configure session
 builder.Services.AddDistributedMemoryCache();
@@ -77,25 +63,7 @@ builder.Services.AddScoped<ISectionServiceProxy, SectionServiceProxy>();
 builder.Services.AddScoped<IQuizServiceProxy, QuizServiceProxy>();
 builder.Services.AddScoped<IExerciseServiceProxy, ExerciseServiceProxy>();
 
-// Configure Razor Views location
-builder.Services.Configure<RazorViewEngineOptions>(opts =>
-{
-    opts.ViewLocationFormats.Add("/Views/Exercise/{0}.cshtml");
-    opts.ViewLocationFormats.Add("~/Views/Exercise/{0}.cshtml");
-});
 
-// Add CORS Configuration
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowFrontend", policyBuilder =>
-    {
-        policyBuilder.WithOrigins("https://localhost:7037")
-                     .AllowAnyHeader()
-                     .AllowAnyMethod()
-                     .AllowCredentials()
-                     .WithExposedHeaders("Content-Type", "Accept");
-    });
-});
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<DataContext>();
@@ -115,7 +83,6 @@ else
     app.UseHsts();
 }
 
-app.UseCors("AllowFrontend");
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -131,34 +98,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.MapControllerRoute(
-    name: "quiz",
-    pattern: "Quiz/{action}/{id}",
-    defaults: new { controller = "Quiz" });
-
-app.MapControllerRoute(
-    name: "exam",
-    pattern: "Exam/{action}/{id}",
-    defaults: new { controller = "Exam" });
-
-app.MapControllerRoute(
-    name: "courses",
-    pattern: "Course/{action=ViewCourses}/{id?}");
-
-app.MapControllerRoute(
-    name: "exercises",
-    pattern: "Exercise/{action=Index}/{id?}",
-    defaults: new { controller = "Exercise" });
-
-app.MapControllerRoute(
-    name: "coursePreview",
-    pattern: "Course/{id?}",
-    defaults: new { controller = "Course", action = "CoursePreview" });
-
-app.MapControllerRoute(
-    name: "module",
-    pattern: "Module/{id:int}",
-    defaults: new { controller = "Module", action = "Details" });
 
 app.MapRazorPages();
 
